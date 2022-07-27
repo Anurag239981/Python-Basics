caff0c9f467ff5f90b1e9bb37f9ac897053b0c45
AnuragGithub <78582214+anu2399811@users.noreply.github.com>
2022-07-27 11:27:45 +0530

    Add files via upload

---
+++ Anurag_pandas.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### pandas - Panel data analysis
+
+# In[ ]:
+
+
+basic python: 1D, hetrogeneous, No broadcasting, Vectorization allowed
+ndarray : ND, homogeneous, Broadcasting allowed, Vectorization allowed
+    
+pandas: two data structures
+    1. Series: 1D, homogeneous, Broadcasting allowed, Vectorization allowed
+    2. DataFrame: 2D, hetrogeneous, Broadcasting allowed, Vectorization allowed
+
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+print(dir(pd))
+
+
+# In[ ]:
+
+
+print(dir(pd.Series))
+
+
+# In[ ]:
+
+
+print(dir(pd.DataFrame))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
